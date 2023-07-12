4c7a1e740a1b846dea5ec5912e14f44194a5aff3
Ignacio Salazar <ignaciosalazar986@gmail.com>
2023-07-12 16:13:09 -0300

    corrigiendo .env

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -1,26 +1,15 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
- /*  apiKey: import.meta.env.VITE_apiKey,
+  apiKey: import.meta.env.VITE_apiKey,
   authDomain: import.meta.env.VITE_authDomain,
   projectId: import.meta.env.VITE_projectId,
   storageBucket: import.meta.env.VITE_storageBucket,
   messagingSenderId: import.meta.env.VITE_messagingSenderId,
-  appId: import.meta.env.VITE_appId */
-  apiKey: "AIzaSyAe1YQVVDcdvQ6BzDPvFZsO7ZsZn053MMY",
-  authDomain: "sion43235.firebaseapp.com",
-  projectId: "sion43235",
-  storageBucket: "sion43235.appspot.com",
-  messagingSenderId: "1080611099225",
-  appId: "1:1080611099225:web:a113772311377931bd9748"
+  appId: import.meta.env.VITE_appId,
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-export const baseDatos= getFirestore(app)
+export const baseDatos = getFirestore(app);
